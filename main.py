402cd96132f0bb8bd928da68bf4eb3a488f1ef23
Александр <160397385+Antosanja2010@users.noreply.github.com>
2024-03-19 18:22:38 +0200

    Create main.py
    bguherf

---
+++ main.py
@@ -0,0 +1,49 @@
+import discord
+import os
+import random
+from discord.ext import commands
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+
+@bot.command()
+async def mem(ctx):
+    images = os.listdir('images')
+    img_name = random.choice(images)
+    with open(f'images/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+
+
+
+
+
+@bot.command()
+async def animal(ctx):
+    animal = os.listdir('animal')
+    img_name = random.choice(animal)
+    with open(f'animal/{img_name}', 'rb') as f:
+        # В переменную кладем файл, который преобразуется в файл библиотеки Discord!
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+bot.run("Свой токен")
